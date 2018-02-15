e75e28548760e10ae86ca392206f313ac2669468
Berkat Bhatti <beme@theknightmarket.com>
2018-02-15 11:51:43 -0500

    initial commit- login and register UI

---
+++ lyftt/Controller/LoginVC.swift
@@ -0,0 +1,29 @@
+//
+//  LoginVC.swift
+//  lyftt
+//
+//  Created by berkat bhatti on 2/15/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+import UIKit
+
+class LoginVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+//--Actions
+    
+    @IBAction func backedPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+//--Selectors
+    
+    
+
+}
---
+++ lyftt/Controller/signUpVC.swift
@@ -0,0 +1,18 @@
+//
+//  signUpVC.swift
+//  lyftt
+//
+//  Created by berkat bhatti on 2/15/18.
+//  Copyright © 2018 TKM. All rights reserved.
+//
+
+import UIKit
+
+class signUpVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+}
